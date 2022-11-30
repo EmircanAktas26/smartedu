5907cc8bd2cc1dd5236f571a40726855ceff6cae
EmircanAktas26 <emircanaktas26@hotmail.com>
2022-11-30 19:35:16 +0300

    Lesson 06

--- smartedu_com/courses/models.py
+++ smartedu_com/courses/models.py
@@ -12,9 +12,20 @@ class Category(models.Model):
 
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    slug = models.SlugField(max_length=50, unique=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
 class Course(models.Model):
     name = models.CharField(max_length=200, unique=True)
     category = models.ForeignKey(Category, null=True, on_delete=models.DO_NOTHING)
+    tags = models.ManyToManyField(Tag, blank=True)
     descriptation = models.TextField(blank=True, null=True)
     created = models.DateTimeField('date created')
     image = models.ImageField(upload_to="courses/%Y/%m/%d/", default="courses/default_course_image.jpg")
--- smartedu_com/courses/views.py
+++ smartedu_com/courses/views.py
@@ -1,24 +1,53 @@
 from django.shortcuts import render
-from . models import Course
+from . models import Course,Category,Tag
 
 # Create your views here.
 
 def course_list(request):
-    courses = Course.objects.all().order_by('-created')
-
+    courses = Course.objects.all().order_by('-date')
+    categories = Category.objects.all()
+    tags = Tag.objects.all()
     context = {
-        'courses': courses
+        'courses': courses,
+        'categories':categories,
+        'tags':tags 
     }
-
-
-
+    
     return render(request, 'courses.html',context)
 
 
 def course_detail(request, category_slug, course_id):
     course = Course.objects.get(category__slug = category_slug, id= course_id)
-
+    categories = Category.objects.all()
+    tags = Tag.objects.all()
     context = {
-        'course': course
+        'course': course,
+        'categories':categories,
+        'tags':tags
     }
     return render(request, 'course.html', context)
+
+    
+def category_list(request, category_slug):
+    courses = Course.objects.all().filter(category__slug = category_slug)
+    categories = Category.objects.all()
+    tags = Tag.objects.all()
+
+    context = {
+        'courses': courses,
+        'categories':categories,
+        'tags':tags 
+    }
+    return render(request, 'courses.html', context)
+
+def tag_list(request, tag_slug):
+    courses = Course.objects.all().filter(tags__slug = tag_slug)
+    categories = Category.objects.all()
+    tags = Tag.objects.all()
+
+    context = {
+        'courses': courses,
+        'categories':categories,
+        'tags':tags 
+    }
+    return render(request, 'courses.html', context)

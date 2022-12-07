a26e888de89281777036af5b3f0465d6a01fca5a
EmircanAktas26 <emircanaktas26@hotmail.com>
2022-12-07 04:17:22 +0300

    Lesson 08

--- smartedu_com/pages/urls.py
+++ smartedu_com/pages/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-from pages.views import AboutView,IndexView
+from pages.views import AboutView,IndexView,ContactView
 
 
 
 urlpatterns = [
     path('',IndexView.as_view(),name="index"),
     path('about/',AboutView.as_view(),name="about"),
+    path('contact/',ContactView.as_view(),name="contact"),
     #path(route, view, opt(kısayolismi))
 ]
